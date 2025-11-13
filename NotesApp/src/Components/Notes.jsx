41a30fbf4966a566d15ae33504d63de80c13f03b
shubhzVidhate <shubhamvidhate302@gmail.com>
2025-11-13 13:24:24 +0530

    In this commit  add Desc disply only 5 word

--- NotesApp/src/Components/Notes.jsx
+++ NotesApp/src/Components/Notes.jsx
@@ -2,21 +2,26 @@ import React from 'react'
 
 const Notes = ({ data,handleXCLick }) => {   
 
-    
+    const handleDescriptionText = (des,limit=5) =>{
+        const word = des.split(" ");
+        return word.length > limit ? word.slice(0,limit).join(" ") + "..." : des;
+    }
+
     return (
         <div className="md:w-1/2 sm:w-full  h-full pt-5 pl-5 flex flex-wrap gap-5 overflow-auto">
+            <h4 className='w-full'>Recent Notes</h4>
             {
                data.length >= 1 ?  data.map((item, index) => (
                     <div key={index} className='relative w-40 h-50 bg-white cursor-pointer'>
                         <div className='flex justify-between px-3 pt-2'>
                             <div onClick={()=>handleXCLick(index)} className='h-5 w-5 bg-red-400 text-white rounded-full absolute top-2 right-2 flex items-center justify-center'>X</div>
                             {
-                                item.Important && <small className='bg-amber-400 text-white py-1 px-1 rounded upprcase'>IMP</small>
+                                item.Important ? <small className='bg-amber-400 text-white py-1 px-1 rounded upprcase'>IMP</small> : <small className='mt-6'></small>
                             }
                         </div>
                         <div className='my-3 mx-2'>
                             <h2 className='font-bold text-lg mb-1'>{item.Title}</h2>
-                            <p className='text-gray-500'>{item.Desc}</p>
+                            <p className='text-gray-500'>{handleDescriptionText(item.Desc,5)}</p>
                         </div>
                     </div>
                 )) : <h1 className='text-gray-500'>🫥 No Notes Available</h1>
